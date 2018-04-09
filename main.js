bdb5a4b9ea12e54ea08abf88add2392e1c89495f
Ansh.kapo <ansh605@gmail.com>
2018-04-09 18:45:26 +0530

    Written Some js code with error in changing DOM structure

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-document.getElementById('Add').onclick
+function adder(){
+var value = document.getElementById('list').value;
+if(value){
+creator(value);
+
+
+
+
+}
+
+
+}
+function creator(value){
+var list= document.createElement('li');
+list.innerText=value;
+var parent= document.getElementById('todo');
+parent.appendChild(list);
+var buttons= document.getElementById(buttons);
+buttons.appendChild(list);
+
+
+
+
+
+
+}
